eb00e13df202d132ca6aeb433e37045a2e0b252b
Myo Thaw Tun <myothawtun@aspiremyanmar.com>
2016-08-18 03:50:14 +0000

    Changes made by Myo Thaw Tun (myothawtun@aspiremyanmar.com).

--- PADNAS/components/startView/index.js
+++ PADNAS/components/startView/index.js
@@ -5,21 +5,26 @@ app.startView = kendo.observable({
     afterShow: function() {}
 });
 
+
 // START_CUSTOM_CODE_startView
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
 // END_CUSTOM_CODE_startView
+         /*
 (function(parent) {
     var startViewModel = kendo.observable({
         fields: {
             imageLogo: '',
         },
-        submit: function() {}
+        submit: function() {
+ 
+        }
     });
 
     parent.set('startViewModel', startViewModel);
-})(app.startView);
-
+})
+*/
+(app.startView);
 // START_CUSTOM_CODE_startViewModel
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
