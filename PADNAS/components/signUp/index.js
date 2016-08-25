3c17182f95c5a5b408a31f0ce6774d166cf5529a
Nan Nu Lwin <nannulwin.nnl@gmail.com>
2016-08-25 11:36:52 +0000

    Changes made by Nan Nu Lwin (nannulwin.nnl@gmail.com).

--- PADNAS/components/signUp/index.js
+++ PADNAS/components/signUp/index.js
@@ -1,67 +1,8 @@
 'use strict';
-var everliveApiKey  = new Everlive({
-    appId: 'l2qsxezv534jf65p',
-    scheme: 'https'
-	});
 
-var everliveScheme = 'https' 
 app.signUp = kendo.observable({
     onShow: function() {},
-    afterShow: function() {},
-   initialize:function()
-    {
-       this.bindEvents();
-    },
-    bindEvents:function()
-    {
-      
-        var name="Nan";
-         alert("Binding");
-    document.addEventListener('deviceready', this.onDeviceReady, false);
-	},
-   	onDeviceReady: function() {
-        alert("hey I'm ready");
-    	app.signUp.initializeEverlive();
-   app.signUp.initializeFacebookAuthentication();
-    navigator.splashscreen.hide();
-     },
-    initializeEverlive:function() {
- 	Everlive.init({
-			apiKey: everliveApiKey,
-            scheme: everliveScheme
-		});
-    },
-    initializeFacebookAuthentication: function() {
-        alert("Configuring facebook")
-    var facebookConfig = {
-    name: "Facebook",
-    loginMethodName: "loginWithFacebook",
-    endpoint: "https://www.facebook.com/dialog/oauth",
-    response_type:"token",
-    client_id: '1755726881334128',
-    redirect_uri: 'http://localhost/',
-    access_type:"online",
-    scope:"email",
-    display: "touch"
-    };
-    var facebook = new IdentityProvider(facebookConfig); 
-    var loginFbBtn = document.getElementById("fbButton");
-    loginFbBtn.addEventListener('click', function() {
-    facebook.getAccessToken(function(token) {
-    Everlive.$.Users.loginWithFacebook(token)
-    .then(function(res) {
-    console.log(res);
-    var message = "Welcome to Everlive!";
-    navigator.notification.alert(message, function() {
-    }, "Everlive")
-    }, function(err) {
-    console.log(err);
-    navigator.notification.alert(err.message, function() {
-    }, "Everlive")
-    })
-    })
-    });
-    }
+    afterShow: function() {}
 });
 
 // START_CUSTOM_CODE_signUp
