b1b91c1c6c46403a4409a7bee9b00784854584c1
Myo Thaw Tun <myothawtun@aspiremyanmar.com>
2016-08-25 10:22:30 +0000

    Changes made by Myo Thaw Tun (myothawtun@aspiremyanmar.com).

--- PADNAS/app.js
+++ PADNAS/app.js
@@ -10,7 +10,7 @@
             app.mobileApp = new kendo.mobile.Application(document.body, {
                 transition: 'slide',
                 skin: 'nova',
-                initial: 'components/profile/edit.html'
+                initial: 'components/start/view.html'
             });
         });
     };
